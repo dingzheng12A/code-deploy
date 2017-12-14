c9cf7494dfe5d71e807b39272ff7c9648099a8d6
root <admin@example.com>
2017-12-14 16:51:59 +0800

    修改了路径处理方式

--- shell/read.py
+++ shell/read.py
@@ -4,11 +4,12 @@ from ConfigParser import ConfigParser
 import os
 import sys
 if len(sys.argv) != 3:
-	print "参数不正确"
-	sys.exit(1)
+        print "参数不正确"
+        sys.exit(1)
 
-path=os.path.abspath(os.path.curdir) 
+path=os.path.abspath(os.path.dirname(sys.argv[0]))
+#print "path:%s" %path 
 cf=ConfigParser()
-cf.read('/data/vnnox-deploy/shell/config.ini')
+cf.read(os.path.join(path+'/config.ini'))
 if cf.has_section(sys.argv[1]):
-	print cf.get(sys.argv[1],sys.argv[2])
+        print cf.get(sys.argv[1],sys.argv[2])
